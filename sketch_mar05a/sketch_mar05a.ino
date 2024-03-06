bc144fa7f580509f4867a32aeadee10a9a48aaee
Cristian Usuga <72422960+Cristian171@users.noreply.github.com>
2024-03-05 19:58:49 -0500

    code LED config

--- sketch_mar05a/sketch_mar05a.ino
+++ sketch_mar05a/sketch_mar05a.ino
@@ -4,8 +4,6 @@
 
 #define CLAVE_CORRECTA "1234"
 
-const int ledPin = PICO_DEFAULT_LED_PIN; // Definir el pin del LED integrado
-
 bool verificar_clave(char *secuencia) {
     char clave[6];
     sprintf(clave, "C%s", CLAVE_CORRECTA);
@@ -14,35 +12,40 @@ bool verificar_clave(char *secuencia) {
 
 void salvar_mundo() {
     Serial.println("¡SALVASTE AL MUNDO!");
-    
-    // Encender el LED
-    gpio_init(ledPin);
-    gpio_set_dir(ledPin, GPIO_OUT);
-    gpio_put(ledPin, 1); // 1 enciende el LED
 }
 
 void cuenta_regresiva() {
-    int tiempo = 10;
-    while (tiempo > 0) {
+    Serial.println("Comienza la cuenta regresiva:");
+    for (int tiempo = 10; tiempo > 0; tiempo--) {
         Serial.println(tiempo);
         delay(1000);  // Esperar un segundo
-        tiempo--;
     }
     Serial.println("¡El mundo ha sido destruido!");
-
-    // Apagar el LED
-    gpio_put(ledPin, 0); // 0 apaga el LED
 }
 
 void setup() {
     Serial.begin(9600);
     Serial.println("Inicio del programa");
+    pinMode(LED_BUILTIN, OUTPUT); // Configura el pin del LED incorporado como salida
 }
 
 void loop() {
+    // Parpadeo del LED
+    static uint32_t previousTime = 0;
+    static bool ledState = true;
+
+    uint32_t currentTime = millis();
+
+    if( (currentTime - previousTime) > 100){
+        previousTime = currentTime;
+        ledState = !ledState;
+        digitalWrite(LED_BUILTIN, ledState);
+    }
+
+    // Verificar la secuencia ingresada por el usuario
     char secuencia[6];
     Serial.readBytes(secuencia, 6);
-    
+
     if (secuencia[0] == 'C' && verificar_clave(secuencia)) {
         salvar_mundo();
     } else {
